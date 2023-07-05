97cf06cd8c14b24c7bb44c6f700e09c40abb90eb
Serhii <brdnk.srh@gmail.com>
2023-07-05 15:12:18 +0300

    first commit

---
+++ components/GameControls.tsx
@@ -0,0 +1,6 @@
+import { View } from "react-native";
+function GameControls() {
+    return <View></View>;
+}
+
+export default GameControls;
---
+++ components/GameInfo.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet } from "react-native";
+
+function GameInfo() {
+    return <View></View>;
+}
+
+export default GameInfo;
+
+const styles = StyleSheet.create({
+    gameInfoContainer: {},
+});
